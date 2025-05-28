6dc8324714dc3048fe1b8a48bd21fdf7bb3b11d0
ViratJinjala <virat.jinjala@simform.dom>
2025-05-28 14:32:42 +0530

    implemented docker

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 from fastapi import FastAPI
 from routers import user_router, todo_router, auth_router
-# from contextlib import asynccontextmanager
-# from utils.init_db import create_table
+from contextlib import asynccontextmanager
+from utils.init_db import create_table
 
 
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    create_table()
+    print("database created!")
+    yield
+    print("Application is shut down.")  
+app =FastAPI(lifespan=lifespan)
 
-# We don't need to use this as we are using alembic
-
-
-# @asynccontextmanager
-# async def lifespan(app:FastAPI):
-# create_table()
-#     print("database created!")
-#     yield
-#     print("Application is shut down.")  
-# app =FastAPI(lifespan=lifespan)
-
-app = FastAPI()
+# app = FastAPI()
 
 app.include_router(user_router.router)
 app.include_router(todo_router.router)
